6cb67374d8d8d31ebae35510cd990fd44fae0247
박상지 <parksangji1109@gmail.com>
2022-02-09 18:39:04 +0900

    [FIX] client form

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,71 @@
-import React , {useState,useEffect} from "react";
+import { useState, useEffect, useRef } from 'react';
+import * as mobilenet from "@tensorflow-models/mobilenet";
 
 function App() {
-  const [data,setData] = useState([{}])
-  useEffect(() => {
-    fetch("/members").then(
-      res => res.json()
-    ).then(
-      data => {
-        setData(data)
-        console.log(data)
-      }
-    )
-  },[])
-  return (
-    <div>
-      {(typeof data.members === 'undefined')? (<p>Loading...</p>) : (
-        data.members.map((member,i) => (
-          <p key= {i}>{member}</p>
-        ))
-      )}
-    </div>
-  );
+    const [isModelLoading, setIsModelLoading] = useState(false)
+    const [model, setModel] = useState(null)
+    const [imageURL, setImageURL] = useState(null);
+
+    const imageRef = useRef()
+    const textInputRef = useRef()
+    const fileInputRef = useRef()
+
+    const loadModel = async () => {
+        setIsModelLoading(true)
+        try {
+            const model = await mobilenet.load()
+            setModel(model)
+            setIsModelLoading(false)
+        } catch (error) {
+            console.log(error)
+            setIsModelLoading(false)
+        }
+    }
+
+    const uploadImage = (e) => {
+        const { files } = e.target
+        if (files.length > 0) {
+            const url = URL.createObjectURL(files[0])
+            setImageURL(url)
+        } else {
+            setImageURL(null)
+        }
+    }
+
+   
+    
+    const triggerUpload = () => {
+        fileInputRef.current.click()
+    }
+
+    useEffect(() => {
+        loadModel()
+    }, [])
+
+
+    if (isModelLoading) {
+        return <h2>Model Loading...</h2>
+    }
+
+    return (
+        <div className="App">
+            <h1 className='header'>Cat vs Dog Classification</h1>
+            <div className='inputHolder'>
+                <input type='file' accept='image/*' capture='camera' className='uploadInput' onChange={uploadImage} ref={fileInputRef} />
+                <button className='uploadImage' onClick={triggerUpload}>Upload Image</button>
+            </div>
+            <div className="mainWrapper">
+                <div className="mainContent">
+                    <div className="imageHolder">
+                        {imageURL && <img src={imageURL} alt="Upload Preview" crossOrigin="anonymous" ref={imageRef} />}
+                    </div>
+                    
+                </div>
+            </div>
+            {imageURL && <button className='button' >Identify Image</button>}
+
+        </div>
+    );
 }
 
 export default App;
